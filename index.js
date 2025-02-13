abf59a30866e85060b7e45bc34ecd5fcb8c6182d
ShrabonKundu <shrabonkundu@gmail.com>
2025-02-13 23:32:25 +0600

    Create UserCOllection and post, get, patch and delete method called

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jod42.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,8 +31,51 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("MasterCafedb").collection("users");
     const menuCollection = client.db("MasterCafedb").collection("menu");
     const reviewsCollection = client.db("MasterCafedb").collection("reviews");
+    const cartCollection = client.db("MasterCafedb").collection("carts");
+
+
+    // user related apis:
+
+    app.post('/users', async(req,res) => {
+      const user = req.body;
+      const query = {email : user.email};
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message : "user already exist" , insertedId : null})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    });
+
+    app.get('/users', async(req,res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    app.delete('/users/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result  = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // create / update  new admin:
+    app.patch('/users/admin/:id', async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set:{
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+    })
+    // 
 
     app.get('/menu', async(req,res) => {
         const result = await menuCollection.find().toArray();
@@ -45,7 +88,27 @@ async function run() {
 
 
 
+    // cart collection 
+    app.post('/carts', async(req,res) => {
+      const cartItem = req.body;
+      console.log(req.body)
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result)
+    })
 
+    app.get('/carts',async(req,res) => {
+      const email =  req.query.email;
+      const query = {email:email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.delete('/carts/:id',async(req,res) => {
+      const id  = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result =  await cartCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
 
